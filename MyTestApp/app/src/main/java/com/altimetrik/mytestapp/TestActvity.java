857ce8908d3f2828ce613e5b2fe51e7ec271995b
hkhan <hkhan@altimetrik.com>
2015-08-11 14:24:36 +0530

    test2

---
+++ MyTestApp/app/src/main/java/com/altimetrik/mytestapp/TestActvity.java
@@ -0,0 +1,46 @@
+package com.altimetrik.mytestapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class TestActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("----hello");
+        System.out.println("----hello2222");
+        System.out.println("----hello333333333");
+
+        System.out.println("----hello444444");
+        System.out.println("----hello55555555555");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
